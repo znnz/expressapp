1095dc584fe2f5d051a8458d1418c730e75934bb
znnz <jojokyi21@gmail.com>
2014-12-05 17:08:15 +0800

    Add API sample

--- meadowlark.js
+++ meadowlark.js
@@ -38,6 +38,9 @@ app.set('etag',function(body,encoding){
 
 app.use(function (req,res,next) {
     console.log(req.ip);
+    if(req.url.match(/^\/(css|js|img|font)\/.+/)){
+        res.set('Cache-Control','public,max-age=3600');
+    }
     res.locals.showTests=app.get('env')!=='production' && req.query.test=='1';
     next();
 });
@@ -47,9 +50,11 @@ app.get('/headers',function(req,res){
     for(var name in req.headers) s+=name+":  "+req,headers[name]+'\n';
     res.send();
 });
+
 app.get('/',function(req,res){
     res.render('home');
 });
+
 
 app.get('/about',function(req,res){
     var fortunes=["Conquer your fears or they will conquer you.","Rivers need springs",
@@ -76,6 +81,71 @@ app.get('/greeting',function(req,res){
    }) ;
 });
 
+app.get('/no-layout',function(req,res){
+    res.render('no-layout',{layout:null});
+})
+
+app.get('/custom-layout',function(req,res){
+   res.render('custom-layout',{layout:'custom'});
+});
+
+var tours=[
+    {id:0,name:'Hood River',price:99.99},
+    {id:1,name:'Oregon Coast',price:149.95}
+];
+
+app.get('/api/tours',function(req,res){
+  var toursXml='<?xml version="1.0"?><tours>'+tours.map(function(p){
+          return '<tour price="'+ p.price+'" id="'+ p.id+'">'+ p.name+'</tour>';
+      }).join('')+'</tours>';
+  var toursText=tours.map(function(p){
+      return p.id+': '+ p.name+' ('+ p.price+')';
+  }).join('\n');
+
+  res.format({
+      'application/json':function(){
+          res.json(tours);
+      },
+      'application/xml':function(){
+          res.type('application/xml');
+          res.send(toursXml);
+      },
+      'text/xml':function(){
+          res.type('text/xml');
+          res.send(toursXml);
+      },
+      'text/plain':function(){
+          res.type('text/plain');
+          res.send(toursXml);
+      }
+  })
+});
+
+app.put('/api/tour/:id',function(req,res){
+   var p=tours.some(function(p){
+       return p.id==req.params.id
+   });
+   if(p){
+       if(req.query.name)p.name=req.query.name;
+       if(req.query.price)p.price=req.query.price;
+       console.log(req.query.name+" ------ "+ req.query.price);
+       res.json({success:true});
+   }else{
+       res.json({error:'No such tour exists.'});
+   }
+});
+
+app.delete('/api/tour/:id',function(req,res){
+   var i;
+    for(i=tours.length-1; i>=0;i--)
+        if(tours[i].id==req.params.id)break;
+    if(i>=0){
+        tours.splice(i,1);
+        res.json({success:true});
+    }else{
+        res.json({error:'No such tour exists.'});
+    }
+});
 app.use(function(req,res,next){
     res.status(404);
     res.render('404');
